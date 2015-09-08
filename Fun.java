bf2d3c99893ccd489e190c980efb12c631ce779d
kyriggs <kriggs01@bellarmine.edu>
2015-09-08 10:25:56 -0400

    Create Fun.java

---
+++ Fun.java
@@ -0,0 +1,12 @@
+/* Fun.java
+Program written by Kyler Riggs
+Due Date: Sept 15, 2015
+*/
+
+public class Fun {
+	public static void main(String [] args) {
+		for ( int i = 0; i < 5; i++ ){
+			System.out.println( "I am having fun." );
+		}
+	}
+}
