64e9546ee9eff440b9cef501dab6ca6fd37c37f0
kyriggs <kriggs01@bellarmine.edu>
2015-09-08 10:27:15 -0400

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,28 @@
+/* Simple.java
+Program written by Kyler Riggs
+Due Date: Sept 15, 2015
+*/
+public class Simple {
+	public static void main(String [] args) {
+		System.out.println("********************************************");
+		System.out.println("*********Programming Assignment #1**********");
+		System.out.println("********Fundamentals of Programming*********");
+		System.out.println("************Author: Kyler Riggs*************");
+		System.out.println("*************Due Date: 9/15/15**************");
+		System.out.println("*************Submitted: 9/8/15**************");
+		System.out.println("********************************************");
+		
+		System.out.println("\n\n		   *    ");
+		System.out.println("	 	  * *    ");
+		System.out.println(" 	 	 * * *    ");
+		
+		System.out.println("\n\n	*************************");
+		System.out.println("	*************************");
+		System.out.println("	*********WELCOME*********");
+		System.out.println("	=========================");
+		System.out.println("	=========================");
+		System.out.println("	**********HOME***********");
+		System.out.println("	*************************");
+		System.out.println("	*************************");
+	}
+}
